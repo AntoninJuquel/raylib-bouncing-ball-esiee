bbb864e410f07aa530e1a8c4716ed9b71cd94447
AntoninJuquel <antonin.juquel@outlook.fr>
2021-11-16 16:28:28 +0100

    Code de base

--- examples/core/core_basic_window.cpp
+++ examples/core/core_basic_window.cpp
@@ -1,23 +1,23 @@
-﻿/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2014 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+﻿	/*******************************************************************************************
+	*
+	*   raylib [core] example - Basic window
+	*
+	*   Welcome to raylib!
+	*
+	*   To test examples, just press F6 and execute raylib_compile_execute script
+	*   Note that compiled executable is placed in the same folder as .c file
+	*
+	*   You can find all basic examples on C:\raylib\raylib\examples folder or
+	*   raylib official webpage: www.raylib.com
+	*
+	*   Enjoy using raylib. :)
+	*
+	*   This example has been created using raylib 1.0 (www.raylib.com)
+	*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+	*
+	*   Copyright (c) 2014 Ramon Santamaria (@raysan5)
+	*
+	********************************************************************************************/
 
 #include "raylib.h"
 #include <raymath.h>
@@ -38,19 +38,224 @@ template <typename T> int sgn(T val) {
 	return (T(0) < val) - (val < T(0));
 }
 
+#pragma region Structs
+struct Cylindrical {
+	float rho;
+	float theta;
+	float y;
+
+	inline Cylindrical operator+(Cylindrical a) {
+		return { a.rho + rho,a.theta + theta,a.y + y };
+	}
+};
+
+struct Spherical {
+	float rho;
+	float theta;
+	float phi;
+
+	inline Spherical operator+(Spherical a) {
+		return { a.rho + rho,a.theta + theta,a.phi + phi };
+	}
+
+};
+#pragma endregion
+
+#pragma region Conversions
+
+Cylindrical CartesianToCylindrical(Vector3 cart)
+{
+	Cylindrical cyl;
+	cyl.rho = sqrtf(cart.x * cart.x + cart.z * cart.z);
+	cyl.y = cart.y;
+
+	if (cyl.rho < EPSILON)cyl.theta = 0;
+	else
+	{
+		cyl.theta = atan2f(cart.x, cart.z);
+		if (cyl.theta < 0)cyl.theta += PI * 2;
+	}
+	return cyl;
+}
+
+Vector3 CylindricalToCartesian(Cylindrical cyl)
+{
+	return Vector3{ cyl.rho * sinf(cyl.theta),cyl.y,cyl.rho * cosf(cyl.theta) };
+}
+
+Vector3 SphericalToCartesian(Spherical sph)
+{
+	return Vector3{ sph.rho * sinf(sph.phi) * sinf(sph.theta),
+	sph.rho * cosf(sph.phi),
+	sph.rho * sinf(sph.phi) * cosf(sph.theta) };
+}
+#pragma endregion
+
+#pragma region Drawers
+void MyDrawSphereEx2(Quaternion q, Vector3 centerPos, float radius, int nSegmentsTheta, int nSegmentsPhi, Color color)
+{
+	if (nSegmentsTheta < 3 || nSegmentsPhi < 2) return;
+
+	std::vector<Vector3> vertexBufferTheta(nSegmentsTheta + 1);
+	std::fill(vertexBufferTheta.begin(), vertexBufferTheta.end(), Vector3{ 0,radius,0 });
+
+	int numVertex = nSegmentsTheta * nSegmentsPhi * 6;
+	if (rlCheckBufferLimit(numVertex)) rlglDraw();
+
+	rlPushMatrix();
+
+	// NOTE: Transformation is applied in inverse order (scale -> translate)
+	rlTranslatef(centerPos.x, centerPos.y, centerPos.z);
+
+	//ROTATION
+	Vector3 vect;
+	float angle;
+	QuaternionToAxisAngle(q, &vect, &angle);
+	rlRotatef(angle * RAD2DEG, vect.x, vect.y, vect.z);
+	//
+
+	rlScalef(radius, radius, radius);
+
+
+	rlBegin(RL_TRIANGLES);
+	rlColor4ub(color.r, color.g, color.b, color.a);
+
+	float deltaPhi = PI / nSegmentsPhi;
+	float deltaTheta = 2 * PI / nSegmentsTheta;
+
+	float phi = 0;
+	for (int i = 0; i < nSegmentsPhi; i++)
+	{
+		float theta = 0;
+		Vector3 tmpBottomLeft = SphericalToCartesian(Spherical{ radius,theta,phi + deltaPhi });
+
+		for (int j = 0; j < nSegmentsTheta; j++)
+		{
+			Vector3 topLeft = vertexBufferTheta[j];
+			Vector3 bottomLeft = tmpBottomLeft;
+			Vector3 topRight = vertexBufferTheta[j + 1];
+			Vector3 bottomRight = SphericalToCartesian(Spherical{ radius,theta + deltaTheta,phi + deltaPhi });
+
+
+			rlVertex3f(bottomLeft.x, bottomLeft.y, bottomLeft.z);
+			rlVertex3f(topRight.x, topRight.y, topRight.z);
+			rlVertex3f(topLeft.x, topLeft.y, topLeft.z);
+
+			rlVertex3f(bottomLeft.x, bottomLeft.y, bottomLeft.z);
+			rlVertex3f(bottomRight.x, bottomRight.y, bottomRight.z);
+			rlVertex3f(topRight.x, topRight.y, topRight.z);
+
+			theta += deltaTheta;
+
+			vertexBufferTheta[j] = tmpBottomLeft;
+			tmpBottomLeft = bottomRight;
+		}
+		vertexBufferTheta[vertexBufferTheta.size() - 1] = vertexBufferTheta[0];
+		phi += deltaPhi;
+	}
+	rlEnd();
+	rlPopMatrix();
+}
+
+void MyDrawSphereWiresEx2(Quaternion q, Vector3 centerPos, float radius, int nSegmentsTheta, int nSegmentsPhi, Color color)
+{
+	if (nSegmentsTheta < 3 || nSegmentsPhi < 2) return;
+
+	std::vector<Vector3> vertexBufferTheta(nSegmentsTheta + 1);
+	std::fill(vertexBufferTheta.begin(), vertexBufferTheta.end(), Vector3{ 0,radius,0 });
+
+	int numVertex = nSegmentsTheta * nSegmentsPhi * 4;
+	if (rlCheckBufferLimit(numVertex)) rlglDraw();
+
+	rlPushMatrix();
+	// NOTE: Transformation is applied in inverse order (scale -> translate)
+	rlTranslatef(centerPos.x, centerPos.y, centerPos.z);
+
+	//ROTATION
+	Vector3 vect;
+	float angle;
+	QuaternionToAxisAngle(q, &vect, &angle);
+	rlRotatef(angle * RAD2DEG, vect.x, vect.y, vect.z);
+	//
+
+	rlScalef(radius, radius, radius);
+
+	rlBegin(RL_LINES);
+	rlColor4ub(color.r, color.g, color.b, color.a);
+
+	float deltaPhi = PI / nSegmentsPhi;
+	float deltaTheta = 2 * PI / nSegmentsTheta;
+
+	float phi = 0;
+	for (int i = 0; i < nSegmentsPhi; i++)
+	{
+		float theta = 0;
+
+		for (int j = 0; j < nSegmentsTheta; j++)
+		{
+			Vector3 topLeft = vertexBufferTheta[j];
+			Vector3 bottomLeft = SphericalToCartesian(Spherical{ radius,theta,phi + deltaPhi });
+			Vector3 topRight = vertexBufferTheta[j + 1];
+
+			rlVertex3f(topLeft.x, topLeft.y, topLeft.z);
+			rlVertex3f(topRight.x, topRight.y, topRight.z);
+
+			rlVertex3f(topLeft.x, topLeft.y, topLeft.z);
+			rlVertex3f(bottomLeft.x, bottomLeft.y, bottomLeft.z);
+
+			theta += deltaTheta;
+
+			vertexBufferTheta[j] = bottomLeft;
+		}
+		vertexBufferTheta[vertexBufferTheta.size() - 1] = vertexBufferTheta[0];
+		phi += deltaPhi;
+	}
+	rlEnd();
+	rlPopMatrix();
+}
+#pragma endregion
+
+#pragma region Intersections
+
+#pragma endregion
+
+#pragma region Methods
 void MyUpdateOrbitalCamera(Camera* camera, float deltaTime)
 {
+	static Spherical sphPos = { 10,PI / 4.f,PI / 4.f };
+	static Spherical sphSpeed = { 10,.4f,.4f };
+	float rhoMin = 4;
+	float rhoMax = 40;
+
+	static Vector2 prevMousePos = { 0,0 };
+	Vector2 mousePos = GetMousePosition();
+	Vector2 mouseVect = Vector2Subtract(mousePos, prevMousePos);
+	prevMousePos = mousePos;
+
+	Spherical sphDelta = { -GetMouseWheelMove() * sphSpeed.rho * deltaTime,
+	IsMouseButtonDown(MOUSE_RIGHT_BUTTON) ? mouseVect.x * sphSpeed.theta * deltaTime : 0,
+	IsMouseButtonDown(MOUSE_RIGHT_BUTTON) ? mouseVect.y * sphSpeed.phi * deltaTime : 0 };
+
+	Spherical newSphPos = sphPos + sphDelta;
+	newSphPos = { Clamp(newSphPos.rho,rhoMin,rhoMax),
+	newSphPos.theta,
+	Clamp(newSphPos.phi,PI / 100.f,.99f * PI) };
+
+	sphPos = newSphPos;
+
+	camera->position = SphericalToCartesian(sphPos);
 }
+#pragma endregion
 
 int main(int argc, char* argv[])
 {
 	// Initialization
 	//--------------------------------------------------------------------------------------
-	float screenSizeCoef = .9f;
+	float screenSizeCoef = 1.4f;
 	const int screenWidth = 1920 * screenSizeCoef;
 	const int screenHeight = 1080 * screenSizeCoef;
 
-	InitWindow(screenWidth, screenHeight, "Cone - Segment Intersection");
+	InitWindow(screenWidth, screenHeight, "Bouncy Sphere");
 
 	SetTargetFPS(60);
 
@@ -64,8 +269,12 @@ int main(int argc, char* argv[])
 	camera.type = CAMERA_PERSPECTIVE;
 	SetCameraMode(camera, CAMERA_CUSTOM);  // Set an orbital camera mode
 
-
-	//--------------------------------------------------------------------------------------
+	//TEST CONVERSION CARTESIAN->CYLINDRICAL
+	Vector3 pos = { 1,1,1 };
+	Cylindrical cyl = CartesianToCylindrical(pos);
+	printf("cyl = (%f,%f,%f) ", cyl.rho, cyl.theta, cyl.y);
+	cyl = cyl + cyl;
+	printf("cyl = (%f,%f,%f) ", cyl.rho, cyl.theta, cyl.y);
 
 	// Main game loop
 	while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -89,7 +298,7 @@ int main(int argc, char* argv[])
 		BeginMode3D(camera);
 		{
 			//
-				
+
 			//3D REFERENTIAL
 			DrawGrid(20, 1.0f);        // Draw a grid
 			DrawLine3D({ 0 }, { 0,10,0 }, DARKGRAY);
@@ -104,9 +313,53 @@ int main(int argc, char* argv[])
 	}
 
 	// De-Initialization
-	//--------------------------------------------------------------------------------------   
+	//--------------------------------------------------------------------------------------  
 	CloseWindow();        // Close window and OpenGL context
 	//--------------------------------------------------------------------------------------
 
 	return 0;
 }
+
+// Draw sphere with extended parameters
+void MyDrawSphereEx(Vector3 centerPos, float radius, int rings, int slices, Color color)
+{
+	int numVertex = (rings + 2) * slices * 6;
+	if (rlCheckBufferLimit(numVertex)) rlglDraw();
+
+	rlPushMatrix();
+	// NOTE: Transformation is applied in inverse order (scale -> translate)
+	rlTranslatef(centerPos.x, centerPos.y, centerPos.z);
+	rlScalef(radius, radius, radius);
+
+	rlBegin(RL_TRIANGLES);
+	rlColor4ub(color.r, color.g, color.b, color.a);
+
+	for (int i = 0; i < (rings + 2); i++)
+	{
+		for (int j = 0; j < slices; j++)
+		{
+			rlVertex3f(cosf(DEG2RAD * (270 + (180 / (rings + 1)) * i)) * sinf(DEG2RAD * (j * 360 / slices)),
+				sinf(DEG2RAD * (270 + (180 / (rings + 1)) * i)),
+				cosf(DEG2RAD * (270 + (180 / (rings + 1)) * i)) * cosf(DEG2RAD * (j * 360 / slices)));
+			rlVertex3f(cosf(DEG2RAD * (270 + (180 / (rings + 1)) * (i + 1))) * sinf(DEG2RAD * ((j + 1) * 360 / slices)),
+				sinf(DEG2RAD * (270 + (180 / (rings + 1)) * (i + 1))),
+				cosf(DEG2RAD * (270 + (180 / (rings + 1)) * (i + 1))) * cosf(DEG2RAD * ((j + 1) * 360 / slices)));
+			rlVertex3f(cosf(DEG2RAD * (270 + (180 / (rings + 1)) * (i + 1))) * sinf(DEG2RAD * (j * 360 / slices)),
+				sinf(DEG2RAD * (270 + (180 / (rings + 1)) * (i + 1))),
+				cosf(DEG2RAD * (270 + (180 / (rings + 1)) * (i + 1))) * cosf(DEG2RAD * (j * 360 / slices)));
+
+			rlVertex3f(cosf(DEG2RAD * (270 + (180 / (rings + 1)) * i)) * sinf(DEG2RAD * (j * 360 / slices)),
+				sinf(DEG2RAD * (270 + (180 / (rings + 1)) * i)),
+				cosf(DEG2RAD * (270 + (180 / (rings + 1)) * i)) * cosf(DEG2RAD * (j * 360 / slices)));
+			rlVertex3f(cosf(DEG2RAD * (270 + (180 / (rings + 1)) * (i))) * sinf(DEG2RAD * ((j + 1) * 360 / slices)),
+				sinf(DEG2RAD * (270 + (180 / (rings + 1)) * (i))),
+				cosf(DEG2RAD * (270 + (180 / (rings + 1)) * (i))) * cosf(DEG2RAD * ((j + 1) * 360 / slices)));
+			rlVertex3f(cosf(DEG2RAD * (270 + (180 / (rings + 1)) * (i + 1))) * sinf(DEG2RAD * ((j + 1) * 360 / slices)),
+				sinf(DEG2RAD * (270 + (180 / (rings + 1)) * (i + 1))),
+				cosf(DEG2RAD * (270 + (180 / (rings + 1)) * (i + 1))) * cosf(DEG2RAD * ((j + 1) * 360 / slices)));
+		}
+	}
+	rlEnd();
+	rlPopMatrix();
+}
+
